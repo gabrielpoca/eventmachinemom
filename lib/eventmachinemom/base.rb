16c438c47684a16e0d091c6f36b0f16e4fdf452f
Gabriel Poça <gabrielpoca@gmail.com>
2013-06-15 23:19:50 +0100

    fixing

--- lib/eventmachinemom/base.rb
+++ lib/eventmachinemom/base.rb
@@ -1,8 +1,3 @@
-# Module Base has essencial class methods
-# FEATURES:
-# 1.  Adds instances variable to class to store all class objects.
-#     Class should be created with method 'create'
-# 2.  Adds method 'all' to return the objects array.
 module Base
   def self.extended(base)
     class << base
